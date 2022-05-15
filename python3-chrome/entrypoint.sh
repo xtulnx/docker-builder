dbe94545eee2967b0f5c94fe11878de11aff627f
jason.liao <xtulnx@126.com>
2022-05-15 12:28:57 +0800

    test

---
+++ python3-chrome/entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -ex
+
+# RUN_FLUXBOX=${RUN_FLUXBOX:-yes}
+RUN_XTERM=${RUN_XTERM:-yes}
+# VNC_PASSWORD=${VNC_PASSWORD:-}
+
+# case $RUN_FLUXBOX in
+#   false|no|n|0)
+#     rm -f /app/conf.d/fluxbox.conf
+#     ;;
+# esac
+
+case $RUN_XTERM in
+  false|no|n|0)
+    rm -f /app/conf.d/xterm.conf
+    ;;
+esac
+
+# if [ -n "$VNC_PASSWORD" ]; then
+#   x11vnc -storepasswd "${VNC_PASSWORD}" /app/conf.d/x11vnc.pass
+#   sed -i '/^command=/s;$; -rfbauth /app/conf.d/x11vnc.pass;' /app/conf.d/x11vnc.conf
+# fi
+
+exec supervisord -c /app/supervisord.conf
