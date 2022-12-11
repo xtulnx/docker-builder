ed8017be0f7f4fb9ab1eba003f4cd5f9de270a3a
jason.liao <xtulnx@126.com>
2022-12-11 21:37:40 +0800

    update: chrome

--- python3-chrome/install.py
+++ python3-chrome/install.py
@@ -1,10 +1,11 @@
 import sys, os
 from undetected_chromedriver.patcher import Patcher
 exec_path= os.environ.get('CHROME_DRIVER',"/app/chromedriver") if len(sys.argv) <= 1 else sys.argv[1]
-version_main=101 if len(sys.argv) <= 2 else int(sys.argv[2])
+version_main=108 if len(sys.argv) <= 2 else int(sys.argv[2])
 patcher = Patcher(exec_path, version_main=version_main)
 release = patcher.fetch_release_number()
 patcher.version_main = release.version[0]
 patcher.version_full = release
 patcher.unzip_package(patcher.fetch_package())
 patcher.patch()
+
